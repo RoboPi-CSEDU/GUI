2cb4d35d7bba196771d254b5120e9034e8b6a76b
tanvirs27 <rifat.csedu20@gmail.com>
2017-01-28 00:21:03 +0600

    custom blocks added

---
+++ robopi/js/block_sensor.js
@@ -0,0 +1,32 @@
+Blockly.Blocks['sensor_get_distance'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("get distance from sensor");
+    this.setOutput(true, "Number");
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['sensor_is_obstacle'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("is obstacle");
+    this.setOutput(true, "Boolean");
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['sensor_get_color'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("get color");
+    this.setOutput(true, "String");
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
---
+++ robopi/js/code_sensor.js
@@ -0,0 +1,20 @@
+Blockly.Python['sensor_get_distance'] = function(block) {
+  
+  var code = 'getDistanceFromSensor()';
+  
+  return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['sensor_is_obstacle'] = function(block) {
+  
+  var code = 'isObstacle()';
+  
+  return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['sensor_get_color'] = function(block) {
+  
+  var code = 'getColorFromSensor()';
+  
+  return [code, Blockly.Python.ORDER_NONE];
+};
